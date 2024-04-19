4ecd089ac9f3774a24dc36ccb9c8594f7643f0bc
root <root@WHEEEE.localdomain>
2024-04-19 14:46:32 -0700

    small update

--- Prog1.py
+++ Prog1.py
@@ -2,20 +2,45 @@ def parser(input_string):
     stack = ['$']  # Initialize stack with '$'
     stack.append('E')  # Push start symbol 'E' onto stack
     input_index = 0
+    top = stack[-1]
+    output = None
+    
+    while stack:
+        print("Stack:", stack, "Input:'" + input_string + "'", "Output:", top, "->", output)
+        output = None
+        top = stack[-1]
+        input = input_string[input_index]
+        if top == input:
+            stack.pop()
+            input_string = input_string[1:]
+            continue
+        elif top == 'ɛ':
+            stack.pop()
+            continue
+        elif top in parsing_table and input in parsing_table[top]:
+            output = parsing_table[top][input]
+        else:
+            return False
+        stack.pop()
+        stack.extend(reversed(output))
 
     
+    return True
 
 
 parsing_table = {
-    'E': {'a': ['TQ'], '(': ['TQ']},
-    'Q': {'+': ['+TQ'], '-': ['-TQ'], ')': ['ɛ'], '$': ['ɛ']},
-    'T': {'a': ['FR'], '(': ['FR']},
-    'R': {'*': ['*FR'], '/': ['/FR'], '+': ['ɛ'], '-': ['ɛ'], ')': ['ɛ'], '$': ['ɛ']},
-    'F': {'a': ['a'], '(': ['(E)']}
+    'E': {'a': ['T','Q'], '(': ['T','Q']},
+    'Q': {'+': ['+','T','Q'], '-': ['-','T','Q'], ')': ['ɛ'], '$': ['ɛ']},
+    'T': {'a': ['F','R'], '(': ['F','R']},
+    'R': {'*': ['*','F','R'], '/': ['/','F','R'], '+': ['ɛ'], '-': ['ɛ'], ')': ['ɛ'], '$': ['ɛ']},
+    'F': {'a': ['a'], '(': ['(','E',')']}
 }
 
 # Test input strings
 input_strings = [
-    "(a+a) $",
-    "(a+a) e $",
+    "(a+a)$",
+    "(a+a)e$",
 ]
+
+print("Test 01:",input_strings[0], parser(input_strings[0]))
+print("Test 02:",input_strings[1], parser(input_strings[1]))
